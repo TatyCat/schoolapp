eeac054e8f3194f3cd2c4a22282585f3250403a4
TatyCat <williams.tatyanna@gmail.com>
2019-06-05 11:56:57 -0400

    Added header for Student and Teacher Pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import SchoolsHomePg from './pages/SchoolsHomePg'
 import SelectedSchool from './pages/SelectedSchoolPG';
 import TeachersPg from './pages/TeachersPg'
+import StudentsPg from './pages/StudentsPg';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route path="/schools" exact component={SchoolsHomePg} />
             <Route path="/schools/:school" component={SelectedSchool} />
             <Route path="/teachers" exact component={TeachersPg} />
+            <Route path="/students" exact component={StudentsPg} />
           </Switch>
         </Router>
       </>
--- src/pages/StudentsPg.js
+++ src/pages/StudentsPg.js
@@ -3,8 +3,7 @@ import Header from '../components/header/Header'
 import HeaderNav from '../components/header/HeaderNav'
 import SchoolsTeachersStudents from '../data/SchoolsTeachersStudents'
 
-
-class TeachersPg extends Component {
+class StudentsPg extends Component {
   state = {
     schools: []
   }
@@ -18,19 +17,10 @@ class TeachersPg extends Component {
       <>
         <Header />
         <HeaderNav />
-        <h1>Teachers:</h1>
-        {this.state.schools.map((schoolList, i) => {
-          return (
-            <ul key={i}>
-              {/* {console.log(schoolList.Teachers[i].Id)} */}
-              {/* <li key={i + "ID"}>{schoolList.Teachers[i]}</li> */}
-              {/* <li key={i + "Name"}>{schoolList.Teachers[i]["Name"]}</li> */}
-            </ul>
-          )
-        })}
+
       </>
     );
   }
 }
 
-export default TeachersPg;
+export default StudentsPg;
